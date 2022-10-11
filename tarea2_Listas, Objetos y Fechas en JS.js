bad4c33b3ad7c0483a339334938b2972d51ebfe7
Gus Rodríguez <43864523+gusrodzu@users.noreply.github.com>
2022-10-11 11:50:15 -0500

    Tarea2 Listas, Objetos y Fechas en JS

---
+++ tarea2_Listas, Objetos y Fechas en JS.js
@@ -0,0 +1,28 @@
+//Nombre
+const nombre = "Gustavo Rodriguez";
+console.log(nombre);
+
+//Edad
+const edad = 34;
+console.log(edad);
+
+//¿Es desarrollador?
+const isDesarrollador = false;
+console.log(isDesarrollador);
+
+//Fecha de nacimiento
+const fecha = new Date("august 12 1988");
+console.log(fecha);
+
+//Libro
+const libro = {
+  titulo: "Balas de plata",
+  autor: "Elmer Mendoza",
+  fecha: new Date("october 26 2013"),
+  url: "https://www.amazon.com.mx/dp/B00G3DOB1C/ref=cm_sw_r_tw_dp_VH4J554RMGFNVN73VSV",
+}
+
+console.log(libro.titulo)
+console.log(libro.autor)
+console.log(libro.fecha)
+console.log(libro.url)
